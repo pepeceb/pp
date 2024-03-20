34691894101e1e50b9d3d92a5da2515484127350
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2024-03-20 20:00:25 +0100

    subo 5 archivos

--- R/cooking.r
+++ R/cooking.r
@@ -1,19 +1,50 @@
-cooking <- function(AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN, FECHA) {
-temp_plot  <-ggplot(data =AC,
+cocinando <- function(AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN, FECHA) {
+
+     colp <- c("A CORUNA" = "steelblue", "SANTA EUGENIA DE RIBEIRA" = "blue", "CILLERO"="darkgreen",
+            "VIGO" = "orange", "AVILES-GIJON" = "darkblue","AVILES"="red", "GIJON"="#00BFC4",
+            "SANTONA" = "#7CAE00", "CEDEIRA"="forestgreen", "FINISTERRE"= "darkgoldenrod2",
+            "LUARCA" = "chartreuse4", "MUROS"= "#619CFF", "CELEIRO"="darkgreen", 
+            "BURELA" ="yellowgreen","SUANCES"="deeppink3",
+            "MARIN"= "mediumorchid", "SAN VICENTE DE LA BARQUERA"= "tomato",
+            "ISLA CRISTINA" ="steelblue", "LLANES"= "darksalmon",
+            "PUNTA UMBRIA" = "slateblue3", "BARBATE"= "red3","SANTANDER"= "red",
+            "PUERTO DE SANTA MARIA"="darkorchid2","ROTA"="orange","A BORDO" = "black",
+            "CADIZ"="Chartreuse2", "TARIFA"= "coral1", "AYAMONTE"= "coral3",
+            "SANLUCAR DE BARRAMEDA"= "darksalmon","PUNTA DEL MORAL"= "red",
+            "CASTLETOWN BERE" = "deeppink3", "PUERTO DE LA VEGA"="black", "MUXIA"="tomato2")    
+         
+         
+         temp_plot  <-ggplot(data =AC,
          mapping = aes(y = cooksd, x=TALLA_MEDIA_MAREA, col=factor(PUERTO2)))  +
-    geom_quasirandom(aes(colour = PUERTO2, size=2,x=TALLA_MEDIA_MAREA, y=cooksd  ),
-                     method = "smiley")  +
+     #geom_point(aes(color = factor(PUERTO2)), position = "jitter",size=3) +  
+     
+     
+     
+    geom_quasirandom(aes(colour = PUERTO2, size=PESO_SP,x=TALLA_MEDIA_MAREA, y=cooksd  ),
+                    method = "smiley")  +
     
     geom_hline(data = AC, aes(yintercept = 4*AC$MN),size=1.5, colour="red")  +
     guides(colour = guide_legend(override.aes = list(size = 3)))     +
     guides(scale= "none",size=FALSE,fill=guide_legend(override.aes=list(size=3))) +
-    scale_size(range=c(2,5))  +
+    scale_size(range=c(4,10))  +
     facet_wrap(ESPECIE~ESTRATO_RIM, scales="free")   +
+ 
+    labs(title=AC$ESPECIE,subtitle="Influential Obs by Cooks distance (cooksd>4*mean)",
+         caption = "AÑO= 2023") +
+    
+    theme_minimal()    +  #theme(legend.position = "none") +
+    theme(strip.text.x = element_text(size=12, angle=0,face="bold", colour="white"),
+          strip.text.y = element_text(size=12, face="bold",colour="white"),
+          strip.background = element_rect(colour="white", fill=c( "steelblue")))  +
+
+
+     
     theme(plot.title = element_text(hjust=0.5,lineheight=7, face="bold", size=16),
           plot.subtitle = element_text(hjust=0.5,lineheight=10, face="bold.italic", 
                                        size = 14)) +
     scale_colour_manual(values=colp,limits = force)   +
-    theme(legend.text = element_text(colour="blue", size = 10, face = "bold"))+
+    theme(legend.text = element_text(colour="steelblue", size = 10, face = "bold"))+
+    theme( legend.title = element_text(colour="blue", size=10, face="bold"))+
     theme(axis.text=element_text(angle=0, size=12, face="bold")) +
     
     geom_blank(aes(x = 0.99*t_min)) + 
@@ -22,12 +53,15 @@ temp_plot  <-ggplot(data =AC,
     
     
     
-    geom_label_repel(show.legend=FALSE,data=subset(AC,cooksd>4*MN),aes(fontface="bold", 
-                          TALLA_MEDIA_MAREA,cooksd, 
+geom_label_repel(show.legend=FALSE,data=subset(AC,  EJEM_MED_MAREA>5 ),
+aes(fontface="bold",  TALLA_MEDIA_MAREA,cooksd, 
 label = ifelse(cooksd>4*MN,paste(round(TALLA_MEDIA_MAREA,2), "cm", "\n",
-                          FECHA, " ", "\n",EJEM_MED_MAREA, "Ejemplares"),"")  ,
-                              vjust=0, hjust=0.5))     +
-    guides(colour = guide_legend(override.aes = list(size=5,linetype=4)))
+FECHA, " ", "\n",EJEM_MED_MAREA, "Ejemplares"),"") , vjust=0, hjust=0.5)) 
+    # scale_x_sqrt()
   
-ggsave(temp_plot, file=paste0("2021_plot_TALLAS_MEDIAS ",AC$ESPECIE," ", ".png"), width = 35, height =25, units = "cm")
+ggsave(temp_plot, file=paste0("2023_plot_TALLAS_MEDIAS ",unique(AC$ESPECIE)," ", ".png"),
+       width = 35, height =25, units = "cm")
+
+temp_plot
+     
 }
---
+++ R/densing.R
@@ -0,0 +1,58 @@
+densing <- function(x) {
+library(stringr)
+library(lubridate)
+library(dplyr)
+library(data.table)
+library(janitor)
+library(stringi)
+library(tidyr)
+
+
+  colp <- c("A CORUNA" = "steelblue", "SANTA EUGENIA DE RIBEIRA" = "blue","RIBEIRA" = "blue", "CILLERO"="darkgreen",
+            "VIGO" = "orange", "AVILES-GIJON" = "darkblue","AVILES"="red", "GIJON"="#00BFC4",
+            "SANTONA" = "#7CAE00", "CEDEIRA"="forestgreen", "FINISTERRE"= "darkgoldenrod2",
+            "LUARCA" = "chartreuse4", "MUROS"= "#619CFF", "CELEIRO"="darkgreen",
+            "BURELA" ="yellowgreen","SUANCES"="deeppink3",
+            "MARIN"= "mediumorchid", "SAN VICENTE DE LA BARQUERA"= "tomato",
+            "ISLA CRISTINA" ="steelblue", "LLANES"= "darksalmon",
+            "PUNTA UMBRIA" = "slateblue3", "BARBATE"= "red3","SANTANDER"= "red",
+            "PUERTO DE SANTA MARIA"="darkorchid2","ROTA"="orange","A BORDO" = "black",
+            "CADIZ"="Chartreuse2", "TARIFA"= "coral1", "AYAMONTE"= "coral3",
+            "SANLUCAR DE BARRAMEDA"= "darksalmon","PUNTA DEL MORAL"= "red",
+            "CASTLETOWN BERE" = "deeppink3", "PUERTO DE LA VEGA"="black", "MUXIA"="tomato2")
+
+
+
+
+
+  tallax<-tallax[complete.cases(tallax[c("EJEM_MEDIDOS", "P_MUE_VIVO")]),]
+
+    tallax$PUERTO <- toupper(stri_trans_general(
+    tallax$PUERTO,    "Latin-ASCII"))
+
+
+    espania <- tallax%>% group_by(ESTRATO_RIM,COD_ID,COD_TIPO_MUE,PUERTO,ESP_MUE,ESP_CAT, TALLA) %>%
+    expand(count = seq(1:EJEM_MEDIDOS)) %>% mutate(n=1)
+
+
+
+  head (espania)
+
+ggplot(espania, aes(TALLA, fill=PUERTO)) +
+ # geom_histogram(binwidth = 2.5)+
+  geom_density(aes(y=2.5 * ..count..))  +
+  facet_wrap(.~COD_ID, scales="fixed")   +
+  scale_fill_manual(values=colp)  +
+  theme_bw()+
+  labs(subtitle = espania$ESP_CAT)+
+  scale_y_sqrt()+
+    theme(strip.text.x = element_text(size=10, angle=0,face="bold", colour="white"),
+          strip.text.y = element_text(size=10, face="bold",colour="white"),
+          strip.background = element_rect(colour="white", fill=c( "steelblue")))  +
+    theme(plot.title = element_text(hjust=0.5,lineheight=7, face="bold", size=16),
+          plot.subtitle = element_text(hjust=0.5,lineheight=10, face="bold.italic",
+                                       size = 14)) +
+  ggtitle(espania$ESTRATO_RIM)
+
+
+}
